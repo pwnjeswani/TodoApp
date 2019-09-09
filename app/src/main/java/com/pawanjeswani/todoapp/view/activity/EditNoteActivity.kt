07916a4c89d358e36de94570aef1f7e229834a93
pa1jesw <pawanjes@gmail.com>
2019-09-09 12:39:56 +0530

    added res and made layout files for both activities

---
+++ app/src/main/java/com/pawanjeswani/todoapp/view/activity/EditNoteActivity.kt
@@ -0,0 +1,13 @@
+package com.pawanjeswani.todoapp.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pawanjeswani.todoapp.R
+
+class EditNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
